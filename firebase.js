dd8668fca9d909a8e10c8806f1aa3fba2ddef136
ddavis404 <deilendavis44@hotmail.com>
2024-08-10 18:43:48 -0700

    vercel deployment error again

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,18 +7,15 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyARixSr1dAWO_baMOJJUCa1Eni4S6chodE",
-  authDomain: "inventory-management-5ba2a.firebaseapp.com",
-  projectId: "inventory-management-5ba2a",
-  storageBucket: "inventory-management-5ba2a.appspot.com",
-  messagingSenderId: "250388793488",
-  appId: "1:250388793488:web:70c17f3415b394bc457015",
-  measurementId: "G-8DF5YL109G"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
-
 export { firestore }
